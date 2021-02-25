22c7ea7a52697e1668ac03458bd4d413d3fdd237
catd825 <catd825@gmail.com>
2021-02-25 15:45:18 -0500

    congrats tests complete

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-
-/*
+/**
     *Functional react component for congratulatory message
     * @function
+    * @param {object} props - React props
     * @returns {JSX.Element}
 */
 
-export default () =>{
-    return (
-        <div> 
-
-        </div>
-    )
+export default (props) =>{
+        if(props.success){
+            return(
+            <div data-test="component-congrats"> 
+                <span data-test="congrats-message">
+                    Congratulations! you guessed the word.
+                </span>
+            </div>
+            );
+        } else {
+        return (
+            <div data-test="component-congrats"/>
+        );
+        }
 }
