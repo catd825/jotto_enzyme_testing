e66762aa294797c02fe88b6409cb3a186876ff07
catd825 <catd825@gmail.com>
2021-02-25 14:44:33 -0500

    first commit

---
+++ src/Congrats.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+/*
+    *Functional react component for congratulatory message
+    * @function
+    * @returns {JSX.Element}
+*/
+
+export default () =>{
+    return (
+        <div> 
+
+        </div>
+    )
+}
